94cf9a5c596ce643bc7959a1f88c98b6d01c360d
Woong0810 <chan081010@naver.com>
2025-10-13 22:04:39 +0900

    나루토 전체 애니메이션 출력해보기

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from pico2d import *
+from naruto_frames import FRAMES
 
 open_canvas(800, 600)
-img = load_image('Characters_Naruto.png')
+img = load_image('Characters_Naruto_clean.png')
 
-frames = [
-    {'left': 0, 'bottom': 1938, 'width': 56, 'height': 48},
-    {'left': 58, 'bottom': 1938, 'width': 56, 'height': 48},
-    {'left': 116, 'bottom': 1938, 'width': 56, 'height': 48},
-    {'left': 174, 'bottom': 1938, 'width': 56, 'height': 48},
-    {'left': 0, 'bottom': 1888, 'width': 48, 'height': 48},
-]
 while True:
-    clear_canvas()
-    for frame in frames:
+    for frame in FRAMES:
+        clear_canvas()
         img.clip_draw(frame['left'], frame['bottom'], frame['width'], frame['height'], 400, 300)
         update_canvas()
         delay(0.1)
