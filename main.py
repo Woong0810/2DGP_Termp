850d9f88c784c742f5b1e92fda85b128b5a61915
Woong0810 <chan081010@naver.com>
2025-10-14 13:06:42 +0900

    이타치 캐릭터도 같은 과정 진행

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pico2d import *
-from naruto_frames import FRAMES
+# from naruto_frames import FRAMES
+from characters_itachi_frames import FRAMES
 
 open_canvas(800, 600)
-img = load_image('Characters_Naruto_clean.png')
+# img = load_image('Characters_Naruto_clean.png')
+img = load_image('Characters_Itachi_clean.png')
 
 while True:
     for frame in FRAMES:
