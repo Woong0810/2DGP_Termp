7aa24ad6c712e5612cf0adf13d1d13f6f088ac1f
Woong0810 <chan081010@naver.com>
2025-10-15 19:53:09 +0900

    기본적인 함수들 추가하기. 출력해보기 위해서 state_machine과 기본 상태인 idle class 추가해야됨

--- main.py
+++ main.py
@@ -15,9 +15,44 @@ class Character:
         self.state_machine.update()
     def draw(self):
         self.state_machine.draw()
+        pass
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+running = True
+
+def reset_world():
+    global world
+    global character
+
+    world = []
+    character = Character()
+    world.append(character)
+
+def update_world():
+    for o in world:
+        o.update()
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 open_canvas(800, 600)
 
-
+reset_world()
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
 
 close_canvas()
