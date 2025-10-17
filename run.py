6d280ad993884785e19265ee7cc6f61f69a7a4e9
Woong0810 <chan081010@naver.com>
2025-10-17 22:28:30 +0900

    좌우 달리기를 구현하려면 enter() 인자를 바꿔야할듯

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from character import right_down, right_up, left_down, left_up
 from characters_naruto_frames import FRAMES
 RUN_FRAMES = [FRAMES[i] for i in range(26, 32)]
 
@@ -5,7 +6,10 @@ class Run:
     def __init__(self, naruto):
         self.naruto = naruto
     def enter(self):
-        pass
+        if right_down(e) or left_up(e):
+            self.naruto.dir = self.naruto.face_dir = 1
+        elif left_down(e) or right_up(e):
+            self.naruto.dir = self.naruto.face_dir = -1
     def exit(self):
         pass
     def do(self):
