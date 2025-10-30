dceb90589599d416dd5c23b5eb93a2f0bfbb5318
Woong0810 <chan081010@naver.com>
2025-10-31 03:40:42 +0900

    기본 공격을 눌렀을때 프레임 별로 출력해서 어색하던 문제를 세그먼트 별로 출력해봄

--- normal_attack.py
+++ normal_attack.py
@@ -1,4 +1,4 @@
-from event_to_string import up_down
+from event_to_string import up_down, n_down
 from characters_naruto_frames import FRAMES
 
 NORMAL_ATTACK_FRAME = [FRAMES[i] for i in range(0, 12)]
@@ -11,13 +11,25 @@ class Normal_Attack:
         self.start_frame = 0
         self.end_frame = 0
         self.cur = 0
+        self.segment_playing = False  # 세그먼트 재생 중인지 여부
+        self.next_combo_requested = False  # 재생 중 다음 콤보 요청 여부
 
     def enter(self, e):
+        # n키를 눌렀을 때 새로운 세그먼트 시작
+        if n_down(e):
+            self.start_segment()
+        else:
+            # 이전 콤보에서 이어서 재생
+            self.start_segment()
+
+    def start_segment(self):
         self.start_frame, self.end_frame = SEGMENTS[self.combo_index]
         self.cur = self.start_frame
         self.naruto.frame = self.cur
         self.naruto.accum_time = 0.0
         self.naruto.frame_duration = 0.13
+        self.segment_playing = True
+        self.next_combo_requested = False
 
     def exit(self, e):
         if up_down(e):
@@ -27,6 +39,9 @@ class Normal_Attack:
         if self.naruto.JUMP.active:
             return
 
+        if not self.segment_playing:
+            return
+
         self.naruto.accum_time += dt
         if self.naruto.accum_time >= self.naruto.frame_duration:
             self.naruto.accum_time -= self.naruto.frame_duration
@@ -34,10 +49,24 @@ class Normal_Attack:
                 self.cur += 1
                 self.naruto.frame = self.cur
             else:
-                self.combo_index = (self.combo_index + 1) % len(SEGMENTS)
-                self.start_frame, self.end_frame = SEGMENTS[self.combo_index]
-                self.cur = self.start_frame
-                self.naruto.frame = self.cur
+                # 세그먼트 재생 완료
+                self.segment_playing = False
+
+                if self.next_combo_requested:
+                    # 재생 중 n키가 눌렸다면 다음 콤보로
+                    self.combo_index = (self.combo_index + 1) % len(SEGMENTS)
+                    self.start_segment()
+                else:
+                    self.combo_index = 0  # 콤보 초기화
+                    self.naruto.state_machine.add_event(('SEGMENT_END', None))
+
+    def handle_n_key(self):
+        if self.segment_playing:
+            # 세그먼트 재생 중이면 다음 콤보 예약
+            self.next_combo_requested = True
+        else:
+            self.combo_index = (self.combo_index + 1) % len(SEGMENTS)
+            self.start_segment()
 
     def draw(self):
         frame = NORMAL_ATTACK_FRAME[self.naruto.frame]
