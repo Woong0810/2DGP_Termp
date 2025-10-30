c670825df1a8bcb9d4e121e737ef1b9bf779f82f
Woong0810 <chan081010@naver.com>
2025-10-31 03:28:22 +0900

    기존의 main.py를 하나의 게임 모드로 변경

---
+++ play_mode.py
@@ -0,0 +1,35 @@
+import time
+from pico2d import *
+
+import game_world
+from character import Character
+# from characters_itachi_frames import FRAMES
+# from characters_jiraiya_frames import FRAMES
+import game_framework
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            character.handle_event(event)
+
+def init():
+    global character
+
+    character = Character()
+    game_world.add_object(character, 0)
+
+def update(dt):
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    pass
