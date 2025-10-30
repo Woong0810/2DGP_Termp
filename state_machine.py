5bcad0541bb93f034cbb4ba39e941761cf6edede
Woong0810 <chan081010@naver.com>
2025-10-31 04:08:44 +0900

    이전 단계가 RUN이면 포물선 점프, IDLE이면 수직 점프를 할 수 있게 이전 상태도 저장

--- state_machine.py
+++ state_machine.py
@@ -3,6 +3,7 @@ from event_to_string import event_to_string
 class StateMachine:
     def __init__(self, initial_state, rules):
         self.cur_state = initial_state
+        self.prev_state = None  # 이전 상태 저장
         self.rules = rules
         self.cur_state.enter(('START', None))
 
@@ -21,8 +22,9 @@ class StateMachine:
             if check_event(state_event):
                 next_state = self.rules[self.cur_state][check_event]
                 self.cur_state.exit(state_event)
-                next_state.enter(state_event)
-                print(f'{self.cur_state.__class__.__name__} ============== {event_to_string(state_event)} =============> {next_state.__class__.__name__}')
+                self.prev_state = self.cur_state  # 이전 상태 저장
                 self.cur_state = next_state
+                next_state.enter(state_event)
+                print(f'{self.prev_state.__class__.__name__} ============== {event_to_string(state_event)} =============> {next_state.__class__.__name__}')
                 return
         print(f'처리되지 않은 이벤트 {event_to_string(state_event)}가 있습니다')
