c424180cae8c551db64e33f7f3283b5fc691c088
Woong0810 <chan081010@naver.com>
2025-10-31 12:12:42 +0900

    실드 이펙트 프레임 정보

---
+++ shield_effect_frames.py
@@ -0,0 +1,11 @@
+# Auto-generated frame data
+FRAMES = [
+    {'left': 0, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 101, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 202, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 303, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 404, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 505, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 606, 'bottom': 0, 'width': 101, 'height': 100},
+    {'left': 707, 'bottom': 0, 'width': 101, 'height': 100},
+]
