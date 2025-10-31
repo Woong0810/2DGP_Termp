da61f222b9a1dcc423917d21cb18525cb74142a0
Woong0810 <chan081010@naver.com>
2025-10-31 12:22:55 +0900

    defense 상태 내부에서 shield effect 호출. 캐릭터보다 뒤에 출력

--- defense.py
+++ defense.py
@@ -1,18 +1,26 @@
 from characters_naruto_frames import FRAMES
+import game_world
+from shield_effect import ShieldEffect
 
 DEFENSE_FRAMES = [FRAMES[i] for i in range(12, 17)]
 
 class Defense:
     def __init__(self, naruto):
         self.naruto = naruto
+        self.shield_effect = None
 
     def enter(self, e):
         self.naruto.accum_time = 0.0
         self.naruto.frame_duration = 0.1
         self.naruto.frame = 0
+        # create and add shield effect behind the character (layer 0)
+        self.shield_effect = ShieldEffect(self.naruto)
+        game_world.add_object(self.shield_effect, 0)
 
     def exit(self, e):
-        pass
+        if self.shield_effect is not None:
+            game_world.remove_object(self.shield_effect)
+            self.shield_effect = None
 
     def do(self, dt):
         self.naruto.accum_time += dt
