55774b5f03e29c99a8dac7e6e7ed75fd30e70b65
Woong0810 <chan081010@naver.com>
2025-11-03 14:02:22 +0900

    normal_attack도 히트박스 추가

--- normal_attack.py
+++ normal_attack.py
@@ -1,5 +1,6 @@
 from event_to_string import up_down, n_down, n_up
 from characters_naruto_frames import FRAMES
+from pico2d import draw_rectangle
 
 NORMAL_ATTACK_FRAME = [FRAMES[i] for i in range(0, 12)]
 SEGMENTS = [(0, 3), (4, 7), (8, 11)]
@@ -62,3 +63,19 @@ class NormalAttack:
         else:
             self.naruto.image.clip_composite_draw(l, b, w, h, 0.0, 'h',
                                                   self.naruto.x, self.naruto.y, w, h)
+
+    def get_bb(self, scale_x=0.7, scale_y=0.8, x_offset=0, y_offset=0):
+        frame = NORMAL_ATTACK_FRAME[self.naruto.frame]
+        hw = frame['width'] * scale_x / 2
+        hh = frame['height'] * scale_y / 2
+        return (
+            self.naruto.x - hw + x_offset,  # left
+            self.naruto.y - hh + y_offset,  # bottom
+            self.naruto.x + hw + x_offset,  # right
+            self.naruto.y + hh + y_offset   # top
+        )
+
+    def draw_bb(self):
+        # 디버그용: 바운딩 박스를 화면에 그리기
+        left, bottom, right, top = self.get_bb()
+        draw_rectangle(left, bottom, right, top)
