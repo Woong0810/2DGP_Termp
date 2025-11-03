d4d0429cb8672ea14f0be6aab121788071d01aac
Woong0810 <chan081010@naver.com>
2025-11-03 14:10:43 +0900

    defense 도 히트박스 추가, defense는 실드 프레임을 기준으로 히트박스 판정. special attakc은 무적상태. 히트박스 없음

--- defense.py
+++ defense.py
@@ -1,4 +1,5 @@
 from characters_naruto_frames import FRAMES
+from pico2d import draw_rectangle
 import game_world
 from shield_effect import ShieldEffect
 
@@ -39,3 +40,27 @@ class Defense:
         else:
             self.naruto.image.clip_composite_draw(l, b, w, h, 0.0, 'h',
                                                   self.naruto.x, self.naruto.y, w, h)
+
+    def get_bb(self, scale_x=1.0, scale_y=1.0, x_offset=0, y_offset=0):
+        if self.shield_effect is None:
+            return (0, 0, 0, 0)
+
+        frame_w = self.shield_effect.frame_w
+        frame_h = self.shield_effect.frame_h
+
+        draw_w = int(frame_w * 2 / 3)
+        draw_h = int(frame_h * 2 / 3)
+
+        hw = draw_w * scale_x / 2
+        hh = draw_h * scale_y / 2
+        return (
+            self.naruto.x - hw + x_offset,  # left
+            self.naruto.y - hh + y_offset,  # bottom
+            self.naruto.x + hw + x_offset,  # right
+            self.naruto.y + hh + y_offset   # top
+        )
+
+    def draw_bb(self):
+        # 디버그용: 바운딩 박스를 화면에 그리기
+        left, bottom, right, top = self.get_bb()
+        draw_rectangle(left, bottom, right, top)
