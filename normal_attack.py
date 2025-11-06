090c8870d1b76a4025747023b549a1119a3a8b40
Woong0810 <chan081010@naver.com>
2025-11-06 21:06:17 +0900

    normal_attack.py frame_time 기반 수정

--- normal_attack.py
+++ normal_attack.py
@@ -1,5 +1,7 @@
 from event_to_string import up_down, n_down, n_up
 from pico2d import draw_rectangle
+from character_config import ACTION_PER_TIME, NORMAL_ATTACK_ANIMATION_SPEED
+import game_framework
 
 class NormalAttack:
     def __init__(self, character):
@@ -7,43 +9,34 @@ class NormalAttack:
         self.combo_index = 0
         self.start_frame = 0
         self.end_frame = 0
-        self.cur = 0
-        self.n_key_pressed = False  # N키 눌림 상태 추적
+        self.n_key_pressed = False
 
     def enter(self, e):
         # 새로운 세그먼트 시작
         segments = self.character.config.normal_attack_segments
         self.start_frame, self.end_frame = segments[self.combo_index]
-        self.cur = self.start_frame
-        self.character.frame = self.cur
-        self.character.accum_time = 0.0
-        self.character.frame_duration = 0.13
-        # n_down 이벤트로 진입했으면 N키가 눌려있음
+        self.character.frame = self.start_frame
+
         if n_down(e):
             self.n_key_pressed = True
 
     def exit(self, e):
-        # SEGMENT_END로 exit되면 콤보 초기화
         if not n_down(e):
             self.combo_index = 0
         self.n_key_pressed = False
 
-    def do(self, dt):
-        self.character.accum_time += dt
-        if self.character.accum_time >= self.character.frame_duration:
-            self.character.accum_time -= self.character.frame_duration
-            if self.cur < self.end_frame:
-                self.cur += 1
-                self.character.frame = self.cur
+    def do(self):
+        segment_length = self.end_frame - self.start_frame + 1
+        self.character.frame += segment_length * ACTION_PER_TIME * NORMAL_ATTACK_ANIMATION_SPEED * game_framework.frame_time
+
+        if self.character.frame >= self.end_frame + 1:
+            if self.n_key_pressed:
+                self.combo_index = (self.combo_index + 1) % 3
+                segments = self.character.config.normal_attack_segments
+                self.start_frame, self.end_frame = segments[self.combo_index]
+                self.character.frame = self.start_frame
             else:
-                # 세그먼트 재생 완료 - N키가 눌려있는지 확인
-                if self.n_key_pressed:
-                    # N키가 눌려있으면 다음 콤보로
-                    self.combo_index = (self.combo_index + 1) % 3
-                    self.enter(('COMBO_CONTINUE', None))
-                else:
-                    # N키가 안 눌려있으면 IDLE로 복귀
-                    self.character.state_machine.handle_event(('SEGMENT_END', None))
+                self.character.state_machine.handle_event(('SEGMENT_END', None))
 
     def handle_n_key_down(self):
         self.n_key_pressed = True
@@ -55,7 +48,7 @@ class NormalAttack:
         all_frames = self.character.config.frames
         normal_attack_frames = self.character.config.normal_attack_frames
 
-        frame_idx = normal_attack_frames[self.character.frame]
+        frame_idx = normal_attack_frames[int(self.character.frame)]  # int로 변환
         frame = all_frames[frame_idx]
 
         l, b, w, h = frame['left'], frame['bottom'], frame['width'], frame['height']
@@ -73,7 +66,7 @@ class NormalAttack:
         all_frames = self.character.config.frames
         normal_attack_frames = self.character.config.normal_attack_frames
 
-        frame_idx = normal_attack_frames[self.character.frame]
+        frame_idx = normal_attack_frames[int(self.character.frame)]  # int로 변환
         frame = all_frames[frame_idx]
 
         hb = self.character.config.hitbox_normal_attack
@@ -87,6 +80,5 @@ class NormalAttack:
         )
 
     def draw_bb(self):
-        # 디버그용: 바운딩 박스를 화면에 그리기
         left, bottom, right, top = self.get_bb()
         draw_rectangle(left, bottom, right, top)
