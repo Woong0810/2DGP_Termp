1138bd46c37762a1b19f24b814f09d3ad9bf445f
Woong0810 <chan081010@naver.com>
2025-11-06 21:16:23 +0900

    special_attack.py frame_time 기반 수정. 느리게 출력

--- special_attack.py
+++ special_attack.py
@@ -1,48 +1,42 @@
 from pico2d import draw_rectangle
+from character_config import ACTION_PER_TIME, SPECIAL_ATTACK_ANIMATION_SPEED, SPECIAL_ATTACK_LOOP_COUNT
+import game_framework
 
 class SpecialAttack:
     def __init__(self, character):
         self.character = character
-        self.loop_count = 0  # 마지막 4프레임 반복 횟수
+        self.loop_count = 0
 
     def enter(self, e):
-        self.character.accum_time = 0.0
-        self.character.frame_duration = 0.15
         self.character.frame = 0
         self.loop_count = 0
 
     def exit(self, e):
         pass
 
-    def do(self, dt):
-        self.character.accum_time += dt
-        if self.character.accum_time >= self.character.frame_duration:
-            self.character.accum_time -= self.character.frame_duration
+    def do(self):
+        special_frames = self.character.config.special_attack_frames
+        last_4_start = len(special_frames) - 4
+        skip_before_last_4 = last_4_start - 3
 
-            special_frames = self.character.config.special_attack_frames
-            last_4_start = len(special_frames) - 4
-            skip_before_last_4 = last_4_start - 3
+        self.character.frame += len(special_frames) * ACTION_PER_TIME * SPECIAL_ATTACK_ANIMATION_SPEED * game_framework.frame_time
 
-            if self.character.frame < skip_before_last_4 - 1:
-                self.character.frame += 1
-            elif self.character.frame == skip_before_last_4 - 1:
+        # 스킵할 구간에 도달하면 마지막 4프레임으로 점프
+        if skip_before_last_4 - 3 <= self.character.frame < last_4_start:
+            self.character.frame = last_4_start
+
+        # 마지막 프레임 도달 시 반복
+        if self.character.frame >= len(special_frames):
+            if self.loop_count < SPECIAL_ATTACK_LOOP_COUNT:
                 self.character.frame = last_4_start
-            elif self.character.frame < len(special_frames) - 1:
-                # 마지막 4프레임 구간: 진행
-                self.character.frame += 1
+                self.loop_count += 1
             else:
-                # 마지막 프레임에 도달
-                if self.loop_count < 3:
-                    self.character.frame = last_4_start
-                    self.loop_count += 1
-                else:
-                    # 반복 완료: IDLE로 복귀
-                    self.character.state_machine.handle_event(('SPECIAL_ATTACK_END', None))
+                self.character.state_machine.handle_event(('SPECIAL_ATTACK_END', None))
 
     def draw(self):
         special_attack_frames = self.character.config.special_attack_frames
         all_frames = self.character.config.frames
-        frame_idx = special_attack_frames[self.character.frame]
+        frame_idx = special_attack_frames[int(self.character.frame)]  # int로 변환
         frame = all_frames[frame_idx]
 
         l, b, w, h = frame['left'], frame['bottom'], frame['width'], frame['height']
@@ -57,11 +51,10 @@ class SpecialAttack:
                                                   self.character.x, draw_y, draw_w, draw_h)
 
     def get_bb(self):
-        # 120번 프레임부터만 히트박스 설정
         if self.character.frame >= 120:
             special_attack_frames = self.character.config.special_attack_frames
             all_frames = self.character.config.frames
-            frame_idx = special_attack_frames[self.character.frame]
+            frame_idx = special_attack_frames[int(self.character.frame)]  # int로 변환
             frame = all_frames[frame_idx]
 
             hb = self.character.config.hitbox_special_attack
@@ -73,9 +66,8 @@ class SpecialAttack:
                 self.character.x + hw + hb['x_offset'],
                 self.character.y + hh + hb['y_offset']
             )
-        return (0, 0, 0, 0)  # 히트박스 없음
+        return (0, 0, 0, 0)
 
     def draw_bb(self):
-        # 디버그용: 바운딩 박스를 화면에 그리기
         left, bottom, right, top = self.get_bb()
         draw_rectangle(left, bottom, right, top)
