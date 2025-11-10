e574f7b04f03f5a39526b3726a70e22648c5386e
Woong0810 <chan081010@naver.com>
2025-11-10 14:38:57 +0900

    player_mode 수정

--- play_mode.py
+++ play_mode.py
@@ -18,19 +18,18 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_j:
             player2.take_hit()
         else:
-            # 1P는 player1, 2P는 player2가 처리
             player1.handle_event(event)
-            # player2.handle_event(event)  # 2P 조작은 나중에 추가
+            player2.handle_event(event)
 
 def init():
     global player1, player2
 
-    # 1P: 나루토 (왼쪽) - 공통 y좌표 사용
-    player1 = Character(NarutoConfig(), x=200, y=90)
+    from player_config import PLAYER1_KEY_BINDINGS, PLAYER2_KEY_BINDINGS
+
+    player1 = Character(NarutoConfig(), key_bindings=PLAYER1_KEY_BINDINGS, x=200, y=90)
     game_world.add_object(player1, 1)
 
-    # 2P: 이타치 (오른쪽) - 나루토와 같은 y좌표
-    player2 = Character(ItachiConfig(), x=600, y=90)
+    player2 = Character(ItachiConfig(), key_bindings=PLAYER2_KEY_BINDINGS, x=600, y=90)
     player2.face_dir = -1  # 왼쪽을 향하도록
     game_world.add_object(player2, 1)
 
