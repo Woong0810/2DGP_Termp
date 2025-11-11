0c2b3875d32045684fb751c6767e7009b6fb8ee3
Woong0810 <chan081010@naver.com>
2025-11-11 16:00:06 +0900

    update에서 할 내용은 update에. w랑 h는 원본 이미지 정보 사용

--- hp_bar.py
+++ hp_bar.py
@@ -6,34 +6,44 @@ class HPBar:
         self.full_image = load_image('full_hp_bar.png')
         self.x = x
         self.y = y
-        self.width = 250
-        self.height = 20
+        self.width = self.empty_image.w
+        self.height = self.empty_image.h
         self.character = character
         self.is_flipped = is_flipped  # Player2는 오른쪽에서 왼쪽으로 감소
 
+        # HP 캐싱 (HP가 변경될 때만 다시 계산)
+        self.prev_hp = -1
+        self.saved_clip_width = self.width
+        self.saved_clip_left = 0
+        self.saved_draw_x = self.x
+
     def update(self):
-        pass
+        if self.character:
+            current_hp = self.character.hp
+            if current_hp != self.prev_hp:
+                self.prev_hp = current_hp
+
+                if self.character.max_hp > 0:
+                    hp_ratio = current_hp / self.character.max_hp
+                else:
+                    hp_ratio = 0.0
+
+                self.saved_clip_width = int(self.width * hp_ratio)
+
+                if self.is_flipped:
+                    self.saved_clip_left = self.width - self.saved_clip_width
+                    self.saved_draw_x = self.x + (self.width - self.saved_clip_width) // 2
+                else:
+                    self.saved_clip_left = 0
+                    self.saved_draw_x = self.x - (self.width - self.saved_clip_width) // 2
 
     def draw(self):
-        self.empty_image.clip_draw(0, 0, self.width - 1, self.height, self.x, self.y)
+        self.empty_image.clip_draw(0, 0, self.width, self.height, self.x, self.y)
 
-        if self.character and self.character.max_hp > 0:
-            hp_ratio = self.character.hp / self.character.max_hp
-        else:
-            hp_ratio = 1.0
-
-        if hp_ratio > 0:
-            hp_width = int(self.width * hp_ratio)
-
-            if self.is_flipped:
-                # Player2: 오른쪽에서 왼쪽으로 감소
-                clip_left = self.width - hp_width
-                draw_x = self.x + (self.width - hp_width) // 2
-                self.full_image.clip_draw(clip_left, 0, hp_width, self.height, draw_x, self.y)
-            else:
-                # Player1: 왼쪽에서 오른쪽
-                draw_x = self.x - (self.width - hp_width) // 2
-                self.full_image.clip_draw(0, 0, hp_width, self.height, draw_x, self.y)
+        if self.saved_clip_width > 0:
+            self.full_image.clip_draw(
+                self.saved_clip_left, 0, self.saved_clip_width,
+                self.height, self.saved_draw_x, self.y)
 
     def handle_collision(self, group, other):
         pass
