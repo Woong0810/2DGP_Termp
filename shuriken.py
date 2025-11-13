bcd262df6125ed524ef8d7c9f7f548b9a087cf47
Woong0810 <chan081010@naver.com>
2025-11-13 16:59:20 +0900

    shuriken.py 추가

---
+++ shuriken.py
@@ -0,0 +1,54 @@
+from pico2d import load_image, draw_rectangle
+import game_framework
+import game_world
+
+class Shuriken:
+    image = None
+    FRAME = {'left': 0, 'bottom': 0, 'width': 26, 'height': 26}
+
+    def __init__(self, owner, x, y, direction, speed=300):
+        if Shuriken.image is None:
+            Shuriken.image = load_image('shuriken.png')
+
+        self.owner = owner
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.speed = speed
+        self.rotation = 0  # 회전 각도
+        self.rotation_speed = 720  # 초당 회전 속도 (도)
+
+        game_world.add_collision_pairs('shuriken:character', self, None)
+
+    def update(self):
+        self.x += self.speed * self.direction * game_framework.frame_time
+        self.rotation += self.rotation_speed * game_framework.frame_time
+
+        if self.x < -50 or self.x > 850:
+            game_world.remove_object(self)
+
+    def draw(self):
+        l, b, w, h = Shuriken.FRAME['left'], Shuriken.FRAME['bottom'], Shuriken.FRAME['width'], Shuriken.FRAME['height']
+
+        self.image.clip_composite_draw(
+            l, b, w, h,
+            self.rotation * 3.141592 / 180.0,
+            '' if self.direction == 1 else 'h',
+            self.x, self.y,
+            w * 2, h * 2
+        )
+
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        size = 25
+        return (
+            self.x - size,
+            self.y - size,
+            self.x + size,
+            self.y + size
+        )
+
+    def handle_collision(self, group, other):
+        pass
+
