cea8a080e5ad48f0de6d4a1b26df52e42c466292
Woong0810 <chan081010@naver.com>
2025-11-13 20:32:14 +0900

    간단하게 수정

--- shield_effect.py
+++ shield_effect.py
@@ -1,28 +1,22 @@
 from pico2d import load_image
-import game_framework
-from character_config import ACTION_PER_TIME, SHIELD_EFFECT_ANIMATION_SPEED
 
 class ShieldEffect:
     def __init__(self, target):
         self.target = target
         self.image = load_image('shield_effect.png')
-        self.frame_count = 8
-        self.frame_w = self.image.w // 8
-        self.frame_h = self.image.h
-        self.frame = 0.0
+        self.width = self.image.w
+        self.height = self.image.h
 
     def update(self):
-        self.frame = (self.frame + self.frame_count * ACTION_PER_TIME * SHIELD_EFFECT_ANIMATION_SPEED * game_framework.frame_time) % self.frame_count
+        pass  # 단일 프레임이므로 업데이트 불필요
 
     def draw(self):
-        l = int(self.frame) * self.frame_w
-        b = 0
         x = self.target.x
         y = self.target.y
-        draw_w = int(self.frame_w * 2 / 3)
-        draw_h = int(self.frame_h * 2 / 3)
+        draw_w = int(self.width * 2 / 3)
+        draw_h = int(self.height * 2 / 3)
 
         if self.target.face_dir == 1:
-            self.image.clip_draw(l, b, self.frame_w, self.frame_h, x, y, draw_w, draw_h)
+            self.image.clip_draw(0, 0, self.width, self.height, x, y, draw_w, draw_h)
         else:
-            self.image.clip_composite_draw(l, b, self.frame_w, self.frame_h, 0.0, 'h', x, y, draw_w, draw_h)
+            self.image.clip_composite_draw(0, 0, self.width, self.height, 0.0, 'h', x, y, draw_w, draw_h)
