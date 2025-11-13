393bafa19c3761afbc49c926f1111859f372f7c2
Woong0810 <chan081010@naver.com>
2025-11-13 19:54:14 +0900

    이유를 찾았음.. exit에서 캐릭터를 삭제하는데 충돌쌍이 같이 삭제됨

--- idle.py
+++ idle.py
@@ -12,6 +12,7 @@ class Idle:
         game_world.add_collision_pairs('normal_attack:character', None, self.character)
         game_world.add_collision_pairs('special_attack:character', None, self.character)
         game_world.add_collision_pairs('ranged_attack:character', None, self.character)
+        game_world.add_collision_pairs('character:shuriken', self.character, None)
 
     def exit(self, e):
         game_world.remove_collision_object(self.character)
