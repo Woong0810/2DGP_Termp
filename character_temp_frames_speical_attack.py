3aa9e52bc80c0b7d34f65e2252ae7488137ce8c1
Woong0810 <chan081010@naver.com>
2025-11-15 05:57:18 +0900

    프레임 정보 다시 수정

--- character_temp_frames_speical_attack.py
+++ character_temp_frames_speical_attack.py
@@ -1,86 +1,128 @@
 FRAMES = [
-    {'left':   2, 'bottom': 9655, 'width': 285, 'height': 235},  # 0
-    {'left': 290, 'bottom': 9655, 'width': 285, 'height': 235},  # 1
-    {'left':   2, 'bottom': 9420, 'width': 285, 'height': 235},  # 2
-    {'left': 290, 'bottom': 9420, 'width': 285, 'height': 235},  # 3
-    {'left':   2, 'bottom': 9185, 'width': 285, 'height': 235},  # 4
-    {'left': 290, 'bottom': 9185, 'width': 285, 'height': 235},  # 5
-    {'left':   2, 'bottom': 8950, 'width': 285, 'height': 235},  # 6
-    {'left': 290, 'bottom': 8950, 'width': 285, 'height': 235},  # 7
-    {'left':   2, 'bottom': 8715, 'width': 285, 'height': 235},  # 8
-    {'left': 290, 'bottom': 8715, 'width': 285, 'height': 235},  # 9
-    {'left':   2, 'bottom': 8480, 'width': 285, 'height': 235},  # 10
-    {'left': 290, 'bottom': 8480, 'width': 285, 'height': 235},  # 11
-    {'left':   2, 'bottom': 8245, 'width': 285, 'height': 235},  # 12
-    {'left': 290, 'bottom': 8245, 'width': 285, 'height': 235},  # 13
-    {'left':   2, 'bottom': 8010, 'width': 285, 'height': 235},  # 14
-    {'left': 290, 'bottom': 8010, 'width': 285, 'height': 235},  # 15
-    {'left':   2, 'bottom': 7775, 'width': 285, 'height': 235},  # 16
-    {'left': 290, 'bottom': 7775, 'width': 285, 'height': 235},  # 17
-    {'left':   2, 'bottom': 7540, 'width': 285, 'height': 235},  # 18
-    {'left': 290, 'bottom': 7540, 'width': 285, 'height': 235},  # 19
-    {'left':   2, 'bottom': 7305, 'width': 285, 'height': 235},  # 20
-    {'left': 290, 'bottom': 7305, 'width': 285, 'height': 235},  # 21
-    {'left':   2, 'bottom': 7070, 'width': 285, 'height': 235},  # 22
-    {'left': 290, 'bottom': 7070, 'width': 285, 'height': 235},  # 23
-    {'left':   2, 'bottom': 6835, 'width': 285, 'height': 235},  # 24
-    {'left': 290, 'bottom': 6835, 'width': 285, 'height': 235},  # 25
-    {'left':   2, 'bottom': 6600, 'width': 285, 'height': 235},  # 26
-    {'left': 290, 'bottom': 6600, 'width': 285, 'height': 235},  # 27
-    {'left':   2, 'bottom': 6365, 'width': 285, 'height': 235},  # 28
-    {'left': 290, 'bottom': 6365, 'width': 285, 'height': 235},  # 29
-    {'left':   2, 'bottom': 6130, 'width': 285, 'height': 235},  # 30
-    {'left': 290, 'bottom': 6130, 'width': 285, 'height': 235},  # 31
-    {'left':   2, 'bottom': 5895, 'width': 285, 'height': 235},  # 32
-    {'left': 290, 'bottom': 5895, 'width': 285, 'height': 235},  # 33
-    {'left':   2, 'bottom': 5660, 'width': 285, 'height': 235},  # 34
-    {'left': 290, 'bottom': 5660, 'width': 285, 'height': 235},  # 35
-    {'left':   2, 'bottom': 5425, 'width': 285, 'height': 235},  # 36
-    {'left': 290, 'bottom': 5425, 'width': 285, 'height': 235},  # 37
-    {'left':   2, 'bottom': 5190, 'width': 285, 'height': 235},  # 38
-    {'left': 290, 'bottom': 5190, 'width': 285, 'height': 235},  # 39
-    {'left':   2, 'bottom': 4955, 'width': 285, 'height': 235},  # 40
-    {'left': 290, 'bottom': 4955, 'width': 285, 'height': 235},  # 41
-    {'left':   2, 'bottom': 4720, 'width': 285, 'height': 235},  # 42
-    {'left': 290, 'bottom': 4720, 'width': 285, 'height': 235},  # 43
-    {'left':   2, 'bottom': 4485, 'width': 285, 'height': 235},  # 44
-    {'left': 290, 'bottom': 4485, 'width': 285, 'height': 235},  # 45
-    {'left':   2, 'bottom': 4250, 'width': 285, 'height': 235},  # 46
-    {'left': 290, 'bottom': 4250, 'width': 285, 'height': 235},  # 47
-    {'left':   2, 'bottom': 4015, 'width': 285, 'height': 235},  # 48
-    {'left': 290, 'bottom': 4015, 'width': 285, 'height': 235},  # 49
-    {'left':   2, 'bottom': 3780, 'width': 285, 'height': 235},  # 50
-    {'left': 290, 'bottom': 3780, 'width': 285, 'height': 235},  # 51
-    {'left':   2, 'bottom': 3545, 'width': 285, 'height': 235},  # 52
-    {'left': 290, 'bottom': 3545, 'width': 285, 'height': 235},  # 53
-    {'left':   2, 'bottom': 3310, 'width': 285, 'height': 235},  # 54
-    {'left': 290, 'bottom': 3310, 'width': 285, 'height': 235},  # 55
-    {'left':   2, 'bottom': 3075, 'width': 285, 'height': 235},  # 56
-    {'left': 290, 'bottom': 3075, 'width': 285, 'height': 235},  # 57
-    {'left':   2, 'bottom': 2840, 'width': 285, 'height': 235},  # 58
-    {'left': 290, 'bottom': 2840, 'width': 285, 'height': 235},  # 59
-    {'left':   2, 'bottom': 2605, 'width': 285, 'height': 235},  # 60
-    {'left': 290, 'bottom': 2605, 'width': 285, 'height': 235},  # 61
-    {'left':   2, 'bottom': 2370, 'width': 285, 'height': 235},  # 62
-    {'left': 290, 'bottom': 2370, 'width': 285, 'height': 235},  # 63
-    {'left':   2, 'bottom': 2135, 'width': 285, 'height': 235},  # 64
-    {'left': 290, 'bottom': 2135, 'width': 285, 'height': 235},  # 65
-    {'left':   2, 'bottom': 1900, 'width': 285, 'height': 235},  # 66
-    {'left': 290, 'bottom': 1900, 'width': 285, 'height': 235},  # 67
-    {'left':   2, 'bottom': 1665, 'width': 285, 'height': 235},  # 68
-    {'left': 290, 'bottom': 1665, 'width': 285, 'height': 235},  # 69
-    {'left':   2, 'bottom': 1430, 'width': 285, 'height': 235},  # 70
-    {'left': 290, 'bottom': 1430, 'width': 285, 'height': 235},  # 71
-    {'left':   2, 'bottom': 1195, 'width': 285, 'height': 235},  # 72
-    {'left': 290, 'bottom': 1195, 'width': 285, 'height': 235},  # 73
-    {'left':   2, 'bottom':  960, 'width': 285, 'height': 235},  # 74
-    {'left': 290, 'bottom':  960, 'width': 285, 'height': 235},  # 75
-    {'left':   2, 'bottom':  725, 'width': 285, 'height': 235},  # 76
-    {'left': 290, 'bottom':  725, 'width': 285, 'height': 235},  # 77
-    {'left':   2, 'bottom':  490, 'width': 285, 'height': 235},  # 78
-    {'left': 290, 'bottom':  490, 'width': 285, 'height': 235},  # 79
-    {'left':   2, 'bottom':  255, 'width': 285, 'height': 235},  # 80
-    {'left': 290, 'bottom':  255, 'width': 285, 'height': 235},  # 81
-    {'left':   2, 'bottom':   20, 'width': 285, 'height': 235},  # 82
-    {'left': 290, 'bottom':   20, 'width': 285, 'height': 235},  # 83
+    # row 0
+    {'left':   2, 'bottom': 9665, 'width': 285, 'height': 233},  # 0
+    {'left': 290, 'bottom': 9665, 'width': 285, 'height': 233},  # 1
+    # row 1
+    {'left':   2, 'bottom': 9429, 'width': 285, 'height': 233},  # 2
+    {'left': 290, 'bottom': 9429, 'width': 285, 'height': 233},  # 3
+    # row 2
+    {'left':   2, 'bottom': 9193, 'width': 285, 'height': 233},  # 4
+    {'left': 290, 'bottom': 9193, 'width': 285, 'height': 233},  # 5
+    # row 3
+    {'left':   2, 'bottom': 8957, 'width': 285, 'height': 233},  # 6
+    {'left': 290, 'bottom': 8957, 'width': 285, 'height': 233},  # 7
+    # row 4
+    {'left':   2, 'bottom': 8721, 'width': 285, 'height': 233},  # 8
+    {'left': 290, 'bottom': 8721, 'width': 285, 'height': 233},  # 9
+    # row 5
+    {'left':   2, 'bottom': 8485, 'width': 285, 'height': 233},  # 10
+    {'left': 290, 'bottom': 8485, 'width': 285, 'height': 233},  # 11
+    # row 6
+    {'left':   2, 'bottom': 8249, 'width': 285, 'height': 233},  # 12
+    {'left': 290, 'bottom': 8249, 'width': 285, 'height': 233},  # 13
+    # row 7
+    {'left':   2, 'bottom': 8013, 'width': 285, 'height': 233},  # 14
+    {'left': 290, 'bottom': 8013, 'width': 285, 'height': 233},  # 15
+    # row 8
+    {'left':   2, 'bottom': 7777, 'width': 285, 'height': 233},  # 16
+    {'left': 290, 'bottom': 7777, 'width': 285, 'height': 233},  # 17
+    # row 9
+    {'left':   2, 'bottom': 7541, 'width': 285, 'height': 233},  # 18
+    {'left': 290, 'bottom': 7541, 'width': 285, 'height': 233},  # 19
+    # row 10
+    {'left':   2, 'bottom': 7305, 'width': 285, 'height': 233},  # 20
+    {'left': 290, 'bottom': 7305, 'width': 285, 'height': 233},  # 21
+    # row 11
+    {'left':   2, 'bottom': 7069, 'width': 285, 'height': 233},  # 22
+    {'left': 290, 'bottom': 7069, 'width': 285, 'height': 233},  # 23
+    # row 12
+    {'left':   2, 'bottom': 6833, 'width': 285, 'height': 233},  # 24
+    {'left': 290, 'bottom': 6833, 'width': 285, 'height': 233},  # 25
+    # row 13
+    {'left':   2, 'bottom': 6597, 'width': 285, 'height': 233},  # 26
+    {'left': 290, 'bottom': 6597, 'width': 285, 'height': 233},  # 27
+    # row 14
+    {'left':   2, 'bottom': 6361, 'width': 285, 'height': 233},  # 28
+    {'left': 290, 'bottom': 6361, 'width': 285, 'height': 233},  # 29
+    # row 15
+    {'left':   2, 'bottom': 6125, 'width': 285, 'height': 233},  # 30
+    {'left': 290, 'bottom': 6125, 'width': 285, 'height': 233},  # 31
+    # row 16
+    {'left':   2, 'bottom': 5889, 'width': 285, 'height': 233},  # 32
+    {'left': 290, 'bottom': 5889, 'width': 285, 'height': 233},  # 33
+    # row 17
+    {'left':   2, 'bottom': 5653, 'width': 285, 'height': 233},  # 34
+    {'left': 290, 'bottom': 5653, 'width': 285, 'height': 233},  # 35
+    # row 18
+    {'left':   2, 'bottom': 5417, 'width': 285, 'height': 233},  # 36
+    {'left': 290, 'bottom': 5417, 'width': 285, 'height': 233},  # 37
+    # row 19
+    {'left':   2, 'bottom': 5181, 'width': 285, 'height': 233},  # 38
+    {'left': 290, 'bottom': 5181, 'width': 285, 'height': 233},  # 39
+    # row 20
+    {'left':   2, 'bottom': 4945, 'width': 285, 'height': 233},  # 40
+    {'left': 290, 'bottom': 4945, 'width': 285, 'height': 233},  # 41
+    # row 21
+    {'left':   2, 'bottom': 4709, 'width': 285, 'height': 233},  # 42
+    {'left': 290, 'bottom': 4709, 'width': 285, 'height': 233},  # 43
+    # row 22
+    {'left':   2, 'bottom': 4473, 'width': 285, 'height': 233},  # 44
+    {'left': 290, 'bottom': 4473, 'width': 285, 'height': 233},  # 45
+    # row 23
+    {'left':   2, 'bottom': 4237, 'width': 285, 'height': 233},  # 46
+    {'left': 290, 'bottom': 4237, 'width': 285, 'height': 233},  # 47
+    # row 24
+    {'left':   2, 'bottom': 4001, 'width': 285, 'height': 233},  # 48
+    {'left': 290, 'bottom': 4001, 'width': 285, 'height': 233},  # 49
+    # row 25
+    {'left':   2, 'bottom': 3765, 'width': 285, 'height': 233},  # 50
+    {'left': 290, 'bottom': 3765, 'width': 285, 'height': 233},  # 51
+    # row 26
+    {'left':   2, 'bottom': 3529, 'width': 285, 'height': 233},  # 52
+    {'left': 290, 'bottom': 3529, 'width': 285, 'height': 233},  # 53
+    # row 27
+    {'left':   2, 'bottom': 3293, 'width': 285, 'height': 233},  # 54
+    {'left': 290, 'bottom': 3293, 'width': 285, 'height': 233},  # 55
+    # row 28
+    {'left':   2, 'bottom': 3057, 'width': 285, 'height': 233},  # 56
+    {'left': 290, 'bottom': 3057, 'width': 285, 'height': 233},  # 57
+    # row 29
+    {'left':   2, 'bottom': 2821, 'width': 285, 'height': 233},  # 58
+    {'left': 290, 'bottom': 2821, 'width': 285, 'height': 233},  # 59
+    # row 30
+    {'left':   2, 'bottom': 2585, 'width': 285, 'height': 233},  # 60
+    {'left': 290, 'bottom': 2585, 'width': 285, 'height': 233},  # 61
+    # row 31
+    {'left':   2, 'bottom': 2349, 'width': 285, 'height': 233},  # 62
+    {'left': 290, 'bottom': 2349, 'width': 285, 'height': 233},  # 63
+    # row 32
+    {'left':   2, 'bottom': 2113, 'width': 285, 'height': 233},  # 64
+    {'left': 290, 'bottom': 2113, 'width': 285, 'height': 233},  # 65
+    # row 33
+    {'left':   2, 'bottom': 1877, 'width': 285, 'height': 233},  # 66
+    {'left': 290, 'bottom': 1877, 'width': 285, 'height': 233},  # 67
+    # row 34
+    {'left':   2, 'bottom': 1641, 'width': 285, 'height': 233},  # 68
+    {'left': 290, 'bottom': 1641, 'width': 285, 'height': 233},  # 69
+    # row 35
+    {'left':   2, 'bottom': 1405, 'width': 285, 'height': 233},  # 70
+    {'left': 290, 'bottom': 1405, 'width': 285, 'height': 233},  # 71
+    # row 36
+    {'left':   2, 'bottom': 1169, 'width': 285, 'height': 233},  # 72
+    {'left': 290, 'bottom': 1169, 'width': 285, 'height': 233},  # 73
+    # row 37
+    {'left':   2, 'bottom':  933, 'width': 285, 'height': 233},  # 74
+    {'left': 290, 'bottom':  933, 'width': 285, 'height': 233},  # 75
+    # row 38
+    {'left':   2, 'bottom':  697, 'width': 285, 'height': 233},  # 76
+    {'left': 290, 'bottom':  697, 'width': 285, 'height': 233},  # 77
+    # row 39
+    {'left':   2, 'bottom':  461, 'width': 285, 'height': 233},  # 78
+    {'left': 290, 'bottom':  461, 'width': 285, 'height': 233},  # 79
+    # row 40
+    {'left':   2, 'bottom':  225, 'width': 285, 'height': 233},  # 80
+    {'left': 290, 'bottom':  225, 'width': 285, 'height': 233},  # 81
+    # row 41
+    {'left':   2, 'bottom':  -11, 'width': 285, 'height': 233},  # 82
+    {'left': 290, 'bottom':  -11, 'width': 285, 'height': 233},  # 83
 ]
