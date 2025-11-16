0f02a139b7e146a421887c14f162897b87dcf243
Woong0810 <chan081010@naver.com>
2025-11-16 18:13:06 +0900

    필살기1 스프라이트와 프레임 정보 따로 추가

---
+++ character_itachi_sa_frames.py
@@ -0,0 +1,94 @@
+FRAMES = [
+    {'left': 0, 'bottom': 1298, 'width': 256, 'height': 48},
+    {'left': 258, 'bottom': 1298, 'width': 256, 'height': 48},
+    {'left': 516, 'bottom': 1298, 'width': 256, 'height': 48},
+
+    {'left': 0, 'bottom': 1248, 'width': 256, 'height': 48},
+    {'left': 258, 'bottom': 1248, 'width': 256, 'height': 48},
+    {'left': 516, 'bottom': 1248, 'width': 256, 'height': 48},
+
+    {'left': 0, 'bottom': 1198, 'width': 256, 'height': 48},
+    {'left': 258, 'bottom': 1198, 'width': 256, 'height': 48},
+    {'left': 516, 'bottom': 1198, 'width': 256, 'height': 48},
+
+    {'left': 0, 'bottom': 1148, 'width': 256, 'height': 48},
+    {'left': 258, 'bottom': 1148, 'width': 256, 'height': 48},
+    {'left': 516, 'bottom': 1148, 'width': 256, 'height': 48},
+
+    {'left': 0, 'bottom': 1106, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 1106, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 1106, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 1064, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 1064, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 1064, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 1022, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 1022, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 1022, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 980, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 980, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 980, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 938, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 938, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 938, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 896, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 896, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 896, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 854, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 854, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 854, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 812, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 812, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 812, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 770, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 770, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 770, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 728, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 728, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 728, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 686, 'width': 264, 'height': 40},
+    # {'left': 266, 'bottom': 686, 'width': 264, 'height': 40},
+    # {'left': 532, 'bottom': 686, 'width': 264, 'height': 40},
+
+    {'left': 0, 'bottom': 556, 'width': 169, 'height': 128},
+    {'left': 171, 'bottom': 556, 'width': 169, 'height': 128},
+    {'left': 342, 'bottom': 556, 'width': 169, 'height': 128},
+    {'left': 513, 'bottom': 556, 'width': 169, 'height': 128},
+    {'left': 684, 'bottom': 556, 'width': 169, 'height': 128},
+
+    {'left': 0, 'bottom': 426, 'width': 169, 'height': 128},
+
+    {'left': 0, 'bottom': 272, 'width': 131, 'height': 152},
+    {'left': 133, 'bottom': 272, 'width': 131, 'height': 152},
+    {'left': 266, 'bottom': 272, 'width': 131, 'height': 152},
+    {'left': 399, 'bottom': 272, 'width': 131, 'height': 152},
+    {'left': 532, 'bottom': 272, 'width': 131, 'height': 152},
+    {'left': 665, 'bottom': 272, 'width': 131, 'height': 152},
+
+    {'left': 0, 'bottom': 118, 'width': 120, 'height': 152},
+    {'left': 122, 'bottom': 118, 'width': 120, 'height': 152},
+    {'left': 244, 'bottom': 118, 'width': 120, 'height': 152},
+
+    {'left': 0, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 122, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 244, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 366, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 488, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 610, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 732, 'bottom': 60, 'width': 120, 'height': 56},
+    {'left': 854, 'bottom': 60, 'width': 120, 'height': 56},
+
+    {'left': 0,   'bottom': 2, 'width': 120, 'height': 56},
+    {'left': 122, 'bottom': 2, 'width': 120, 'height': 56},
+    {'left': 244, 'bottom': 2, 'width': 120, 'height': 56},
+    {'left': 366, 'bottom': 2, 'width': 120, 'height': 56},
+]
