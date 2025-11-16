fb97a067ec90416edcee603973e657fd6eb91b2c
Woong0810 <chan081010@naver.com>
2025-11-17 06:07:25 +0900

    히트스톱을 위한 히트스톱 타이머 추가

--- game_framework.py
+++ game_framework.py
@@ -3,6 +3,9 @@ import time
 running = None
 stack = None
 
+frame_time = 0.0
+hitstop_time = 0.0
+
 def change_mode(mode):
     global stack
     if (len(stack) > 0):
@@ -39,6 +42,12 @@ def quit():
     global running
     running = False
 
+def add_hitstop(frames):
+    global hitstop_time
+    seconds = frames / 60.0
+    # 이미 더 긴 hitstop이 걸려 있다면 덮어쓰지 않음
+    if seconds > hitstop_time:
+        hitstop_time = seconds
 
 def run(start_mode):
     global running, stack
@@ -46,16 +55,27 @@ def run(start_mode):
     stack = [start_mode]
     start_mode.init()
 
-    global frame_time
+    global frame_time, hitstop_time
     frame_time = 0.0
     current_time = time.time()
     while running:
+        now = time.time()
+        frame_time = now - current_time
+        current_time = now
+
         stack[-1].handle_events()
+
+        if hitstop_time > 0.0:
+            hitstop_time -= frame_time
+            if hitstop_time < 0.0:
+                hitstop_time = 0.0
+            stack[-1].draw()
+            continue
+
         stack[-1].update()
         stack[-1].draw()
-        frame_time = time.time() - current_time
+
         frame_rate = 1.0 / frame_time if frame_time > 0 else 0
-        current_time += frame_time
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
