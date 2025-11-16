b52daf7e91901b95e90c0b24a5a9bd168543e8f5
Woong0810 <chan081010@naver.com>
2025-11-17 06:31:20 +0900

    hit_duration에 맞게 hit 수정

--- hit.py
+++ hit.py
@@ -14,6 +14,7 @@ class Hit:
         self.was_in_air = False
         self.ground_y = 0.0
         self.vy = 0.0
+        self.hit_duration = HIT_DURATION
 
     def enter(self, event):
         self.character.frame = 0
@@ -35,10 +36,12 @@ class Hit:
             self.is_knockback = event[1][0]
             self.knockback_distance = event[1][1] if len(event[1]) > 1 else 0
             self.knockback_dir = event[1][2] if len(event[1]) > 2 else 0
+            self.hit_duration = event[1][3] if len(event[1]) > 3 else HIT_DURATION
         else:
             self.is_knockback = False
             self.knockback_distance = 0
             self.knockback_dir = 0
+            self.hit_duration = HIT_DURATION
         self.vy = 0.0
 
         game_world.add_collision_pairs('normal_attack:character', None, self.character)
@@ -58,15 +61,15 @@ class Hit:
             if self.was_in_air:
                 _, prev_bottom, _, _ = self.get_bb()
 
-            if self.elapsed_time < HIT_DURATION:
+            if self.elapsed_time < self.hit_duration:
                 knockback_frames = self.character.config.knockback_frames
                 frames_per_action = len(knockback_frames)
                 self.character.frame = (self.character.frame + frames_per_action * ACTION_PER_TIME * HIT_ANIMATION_SPEED * game_framework.frame_time) % frames_per_action
 
-                knockback_speed = self.knockback_distance / HIT_DURATION
+                knockback_speed = self.knockback_distance / self.hit_duration
                 self.character.x += self.knockback_dir * knockback_speed * game_framework.frame_time
 
-            elif self.elapsed_time < HIT_DURATION + KNOCKBACK_DOWN_TIME:
+            elif self.elapsed_time < self.hit_duration + KNOCKBACK_DOWN_TIME:
                 if not self.is_lying_down:
                     self.is_lying_down = True
                     # 마지막 넉백 프레임으로 고정
@@ -85,7 +88,7 @@ class Hit:
             frames_per_action = len(hit_frames)
             self.character.frame = (self.character.frame + frames_per_action * ACTION_PER_TIME * HIT_ANIMATION_SPEED * game_framework.frame_time) % frames_per_action
 
-            if self.elapsed_time >= HIT_DURATION:
+            if self.elapsed_time >= self.hit_duration:
                 # 공중에서 피격당했다면 JUMP 상태로 복귀
                 if self.was_in_air:
                     self.character.state_machine.add_event(('RESUME_JUMP', self.ground_y))
