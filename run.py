d4adbc1892536bde16691eb8d9b1c67593b99da7
Woong0810 <chan081010@naver.com>
2025-12-02 15:41:39 +0900

    run.draw도 카메라 적용

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from pico2d import draw_rectangle
 from character_config import RUN_SPEED_PPS, ACTION_PER_TIME, RUN_ANIMATION_SPEED
 import game_framework
 import game_world
+from camera import camera
 
 class Run:
     def __init__(self, character):
@@ -65,19 +66,20 @@ class Run:
     def draw(self):
         run_frames = self.character.config.run_frames
         all_frames = self.character.config.frames
-        frame_idx = run_frames[int(self.character.frame)]  # int로 변환
+        frame_idx = run_frames[int(self.character.frame)]
         frame = all_frames[frame_idx]
 
         l, b, w, h = frame['left'], frame['bottom'], frame['width'], frame['height']
         draw_w = int(w * self.character.config.scale_x)
         draw_h = int(h * self.character.config.scale_y)
-        draw_y = self.character.y + self.character.config.draw_offset_y
+
+        world_y = self.character.y + self.character.config.draw_offset_y
+        sx, sy = camera.world_to_screen(self.character.x, world_y)
 
         if self.character.face_dir == 1:
-            self.character.image.clip_draw(l, b, w, h, self.character.x, draw_y, draw_w, draw_h)
+            self.character.image.clip_draw(l, b, w, h, sx, sy, draw_w, draw_h)
         else:
-            self.character.image.clip_composite_draw(l, b, w, h, 0.0, 'h',
-                                                  self.character.x, draw_y, draw_w, draw_h)
+            self.character.image.clip_composite_draw(l, b, w, h, 0.0, 'h', sx, sy, draw_w, draw_h)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
