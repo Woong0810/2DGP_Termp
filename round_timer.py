bac7c5d095a5abd5bfad3f5d03176fd07d70c38c
Woong0810 <chan081010@naver.com>
2025-12-07 02:23:48 +0900

    폰트 추가, result_mode 삭제, round_timer도 추가한 폰트로 draw

--- round_timer.py
+++ round_timer.py
@@ -4,30 +4,7 @@ import os
 
 class RoundTimer:
     def __init__(self, x, y, round_time=60):
-        # Windows 시스템 폰트 경로 리스트 (굵고 선명한 격투게임 스타일)
-        font_paths = [
-            'PressStart2P.ttf',  # 다운로드한 아케이드 폰트
-            'C:\\Windows\\Fonts\\impact.ttf',  # Impact - 굵고 강렬함
-            'C:\\Windows\\Fonts\\arialbd.ttf',  # Arial Bold
-            'C:\\Windows\\Fonts\\ariblk.ttf',  # Arial Black - 매우 굵음
-            'C:\\Windows\\Fonts\\consola.ttf',  # Consolas - 명확함
-            'C:\\Windows\\Fonts\\cour.ttf',  # Courier - 고정폭
-        ]
-
-        # 사용 가능한 폰트 찾기
-        font_loaded = False
-        for font_path in font_paths:
-            if os.path.exists(font_path):
-                try:
-                    self.font = load_font(font_path, 50)
-                    font_loaded = True
-                    break
-                except:
-                    continue
-
-        # 모든 폰트 로드 실패 시 에러
-        if not font_loaded:
-            raise Exception("폰트를 로드할 수 없습니다. Windows Fonts 폴더를 확인하세요.")
+        self.font = load_font('font.ttf', 50)
 
         self.x = x
         self.y = y
@@ -41,7 +18,7 @@ class RoundTimer:
 
     def draw(self):
         time_int = int(self.time)
-        self.font.draw(self.x - 25, self.y, f'{time_int:02d}', (64, 64, 64))
+        self.font.draw(self.x - 25, self.y, f'{time_int:02d}', (0, 0, 0))
 
     def is_time_over(self):
         return self.time <= 0
